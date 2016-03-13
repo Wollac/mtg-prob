177a976406e6d0c88f888cd759dda04e3908f2db
Wolfgang Welz <welzwo@gmail.com>
2016-03-13 18:25:34 +0100

    Improve hash code computation in Abstract Land

--- src/probability/core/land/AbstractLand.java
+++ src/probability/core/land/AbstractLand.java
@@ -1,7 +1,5 @@
 package probability.core.land;
 
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -11,71 +9,75 @@ import probability.core.Colors;
 
 abstract class AbstractLand implements Land {
 
-	final private String _name;
+    final private String _name;
 
-	final private Set<Color> _colors;
+    final private Colors _colors;
 
-	final private int hashCode;
+    private volatile int _hashCode;
 
-	AbstractLand(String name, Colors colors) {
-		_name = name;
-		_colors = Collections.unmodifiableSet(new HashSet<>(colors
-				.getColors()));
+    AbstractLand(String name, Colors colors) {
+        _name = name;
+        _colors = new Colors(colors);
+    }
 
-		// this class is immutable so we can cache the hash code
-		hashCode = Objects.hash(getClass(), _name, _colors);
-	}
+    @Override
+    final public String getName() {
+        return _name;
+    }
 
-	@Override
-	final public String getName() {
-		return _name;
-	}
+    @Override
+    final public CardType getCardType() {
+        return CardType.Land;
+    }
 
-	@Override
-	final public CardType getCardType() {
-		return CardType.Land;
-	}
+    @Override
+    final public Set<Color> colors() {
+        return _colors.getColors();
+    }
 
-	@Override
-	final public Set<Color> colors() {
-		return _colors;
-	}
+    @Override
+    public Set<Color> producibleColors() {
+        return colors();
+    }
 
-	@Override
-	public Set<Color> producibleColors() {
-		return colors();
-	}
+    @Override
+    public Set<Color> producesColors(Board board) {
+        return colors();
+    }
 
-	@Override
-	public Set<Color> producesColors(Board board) {
-		return colors();
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof AbstractLand)) {
+            return false;
+        }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (!(obj instanceof AbstractLand)) {
-			return false;
-		}
+        AbstractLand other = (AbstractLand) obj;
 
-		AbstractLand other = (AbstractLand) obj;
+        // comparing getClass is bad this should go in each super class
+        return Objects.equals(getClass(), other.getClass())
+                && Objects.equals(_name, other._name)
+                && Objects.equals(_colors, other._colors);
+    }
 
-		// comparing getClass is bad this should go in each super class
-		return Objects.equals(getClass(), other.getClass())
-				&& Objects.equals(_name, other._name)
-				&& Objects.equals(_colors, other._colors);
-	}
+    @Override
+    public int hashCode() {
 
-	@Override
-	public int hashCode() {
-		return hashCode;
-	}
+        int result = _hashCode;
 
-	@Override
-	public String toString() {
-		return getName();
-	}
+        if (result == 0) {
+            result = Objects.hash(getClass(), _name, _colors);
+            _hashCode = result;
+        }
+
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
 
 }
