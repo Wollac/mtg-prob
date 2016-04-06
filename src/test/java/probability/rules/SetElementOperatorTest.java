a46c02e179b50e974d22fb327e4c0d8d6e98e547
Wolfgang Welz <welz@atesio.de>
2016-04-06 10:23:13 +0200

    Add JUnit tests for SetElementOperator

---
+++ src/test/java/probability/rules/SetElementOperatorTest.java
@@ -0,0 +1,73 @@
+package probability.rules;
+
+import com.google.common.collect.Sets;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import probability.attr.ImmutableAttributeHolder;
+import probability.attr.StringSetAttributeKey;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+import java.util.function.BiPredicate;
+
+import static probability.rules.TestUtils.createSingleVariableBinding;
+import static probability.rules.TestUtils.createVariableValueOperatorExpression;
+
+@RunWith(Parameterized.class)
+public class SetElementOperatorTest {
+
+    private static final StringSetAttributeKey KEY = new StringSetAttributeKey("SET");
+    private final Set<String> _varBinding;
+    private final String _value;
+
+    public SetElementOperatorTest(Set<String> varBinding, String value) {
+        _varBinding = varBinding;
+        _value = value;
+    }
+
+    private static void testOperation(Set<String> varBinding, String value, Operation operation, BiPredicate<Set<String>, String> expected) {
+
+        Expression expr;
+        try {
+            expr = createVariableValueOperatorExpression(operation, KEY, value);
+        } catch (Token.RulesTokenException e) {
+            assertValidException(value, e);
+            return;
+        }
+
+        ImmutableAttributeHolder binding = createSingleVariableBinding(KEY, varBinding);
+
+        Assert.assertEquals(expected.test(varBinding, value), expr.interpret(binding));
+    }
+
+    private static void assertValidException(String value, Token.RulesTokenException caughtException) {
+
+        try {
+            if (KEY.parseValue(value).size() == 1) {
+                Assert.fail(caughtException.getMessage());
+            }
+        } catch (Exception e) {
+            Assert.fail(caughtException.getMessage());
+        }
+    }
+
+    @Parameterized.Parameters(name = "{index}: var({0}) operator {1}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {Sets.newHashSet("A", "B"), "A"},
+                {Sets.newHashSet("A", "B"), "[A, B]"},
+                {Collections.emptySet(), ""},
+                {Collections.emptySet(), "A"},
+                {Sets.newHashSet("A", "A"), "B"}
+        });
+    }
+
+    @Test
+    public void testContains() {
+        testOperation(_varBinding, _value, Operation.CONTAINS, Set::contains);
+    }
+}
