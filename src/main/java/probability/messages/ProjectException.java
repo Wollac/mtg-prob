b28704aaa245fc930076d753935b34dbee534217
Wolfgang Welz <welzwo@gmail.com>
2016-09-17 17:22:22 +0200

    Minor cleanups

--- src/main/java/probability/messages/ProjectException.java
+++ src/main/java/probability/messages/ProjectException.java
@@ -11,7 +11,7 @@ public class ProjectException extends Exception {
         INVALID_MULLIGAN_RULE
     }
 
-    private ProjectError _error;
+    private final ProjectError _error;
 
     public ProjectException(ProjectError error) {
 
