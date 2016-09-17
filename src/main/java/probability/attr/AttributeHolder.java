e14bccca7d88567c0eb211a3c7a9596df8b385bb
Wolfgang Welz <welzwo@gmail.com>
2016-09-17 13:45:55 +0200

    Add attribute holder based on properties

--- src/main/java/probability/attr/AttributeHolder.java
+++ src/main/java/probability/attr/AttributeHolder.java
@@ -1,61 +1,23 @@
 package probability.attr;
 
-import java.util.HashMap;
-import java.util.Map;
+interface AttributeHolder extends ImmutableAttributeHolder {
 
-public final class AttributeHolder implements ImmutableAttributeHolder {
+    /**
+     * Sets the value of the corresponding {@link AttributeKey}.
+     *
+     * @param key   the key whose value should be changed
+     * @param value the new value of the corresponding type
+     * @throws IllegalArgumentException if {@link AttributeKey#checkValid(Object)} throws an
+     *                                  exception for the new {@code value}.
+     */
+    <T> void setAttributeValue(AttributeKey<T> key, T value);
 
-    private final Map<AttributeKey<?>, Object> _map;
-
-    public AttributeHolder() {
-        _map = new HashMap<>();
-    }
-
-    public <T> void setAttributeValue(AttributeKey<T> key, T value) {
-
-        try {
-            key.checkValid(value);
-        } catch (AttributeParseException e) {
-            throw new IllegalArgumentException(e);
-        }
-
-        setAttributeValueUnchecked(key, value);
-    }
-
-    public <T> void setAttributeValueUnchecked(AttributeKey<T> key, T value) {
-
-        _map.put(key, value);
-    }
-
-    public <T> void setParsedAttributeValue(AttributeKey<T> key, String valueString)
-            throws AttributeParseException {
-
-        T value = key.parseValue(valueString);
-
-        if (!key.isValid(value)) {
-            throw new AttributeParseException(AttributeParseException.AttributeParseError.INVALID_VALUE, key);
-        }
-
-        setAttributeValueUnchecked(key, value);
-    }
-
-    @Override
-    public <T> T getAttributeValue(AttributeKey<T> key, T def) {
-
-        Object valueObject = _map.get(key);
-        if (valueObject == null) {
-            return def;
-        }
-
-        @SuppressWarnings("unchecked")
-        T value = (T) valueObject;
-
-        return value;
-    }
-
-    @Override
-    public <T> T getAttributeValue(AttributeKey<T> key) {
-        return getAttributeValue(key, key.getDefaultValue());
-    }
-
+    /**
+     * Sets the value of the corresponding {@link AttributeKey}. The new value is not checked for
+     * validity.
+     *
+     * @param key   the key whose value should be changed
+     * @param value the new value of the corresponding type
+     */
+    <T> void setAttributeValueUnchecked(AttributeKey<T> key, T value);
 }
